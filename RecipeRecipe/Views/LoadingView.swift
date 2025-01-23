45c320c7eb0b873c31127704e574cdafc14212d5
oliver <oliverhu.oh@gmail.com>
2025-01-23 15:08:20 -0800

    project updated

---
+++ RecipeRecipe/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  RecipeRecipe
+//
+//  Created by Oliver Hu on 1/23/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
